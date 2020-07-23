2c797bca486ce8134d08c93e81aa1451c59f6203
Sudhir Voleti <sudhir_voleti@isb.edu>
2020-07-23 08:00:19 +0530

    Update ECT an pyfuncs for git.py

--- ECT an pyfuncs for git.py
+++ ECT an pyfuncs for git.py
@@ -79,24 +79,30 @@ def keyphrase_resub(series0):
 # func 2c - text -cleaning
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer,SnowballStemmer
-#stopword_list = nltk.corpus.stopwords.words('english')
+stopword_list = nltk.corpus.stopwords.words('english')
 wnl = WordNetLemmatizer()
-def text_clean0(text):
+def text_clean0(text, lemmatize, stopwords):  # lemmatize = 0 by default
    
-    text = re.sub('<.*?>', '', str(text))    
-    text = re.sub('\d+[,\.]?\d+', '', text)	
-    text = re.sub('-', '_', text)
-    text = re.sub('\$', 'dollar', text)
-    text = re.sub('%', 'percent', text)	
+	text = re.sub('<.*?>', '', str(text))    
+	text = re.sub('\d+[,\.]?\d+', '', text)	
+	text = re.sub('-', '_', text)
+	text = re.sub('\$', 'dollar', text)
+	text = re.sub('%', 'percent', text)	
     
-    text = word_tokenize(str(text))
-    text = [word.lower() for word in text] # lowercase text
-    text = [wnl.lemmatize(i) for i in text]  # lemmatize away plurals
+	text = word_tokenize(str(text))
+	text = [word.lower() for word in text] # lowercase text
+	
+	if lemmatize == 1:
+		text = [wnl.lemmatize(i) for i in text]  # lemmatize away plurals
     
-    text = [word for word in text if word not in stopword_list] # drop stopwords
-    #text = [word for word in text if word.isalpha()] # drop all non-alphabetic tokens  
+	if stopwords == 1:
+		text = [word for word in text if word not in stopword_list] # drop stopwords
 
-    return ' '.join(text)
+	#text = [word for word in text if word.isalpha()] # drop all non-alphabetic tokens  
+
+	return ' '.join(text)
+
+# a0 = list(map(lambda x: text_clean0(x, 0, 0), df01['sents'])) # 0.6 s
 
 
 # func 3a - create & sample from sent-sampling frame. Unit func below
